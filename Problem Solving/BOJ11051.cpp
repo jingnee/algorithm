00f97a4548792edb1584b5bb47c6b93f68c1caec
cho jiyun <jingnee721@gmail.com>
2020-01-24 01:27:48 +0900

    [백준]이항계수2

---
+++ Problem Solving/BOJ11051.cpp
@@ -0,0 +1,20 @@
+//이항계수2 nCk = n-1Ck + n-1Ck-1
+//10007로 나누기
+#include<iostream>
+using namespace std;
+
+int N, K;
+int check[1000][1000];
+
+int binomi(int n, int k) {
+	if (check[n][k] != 0)return check[n][k];
+
+	if (k == 0 || n == k)return check[n][k] = 1;
+	else if (k == 1)return check[n][k] = n;
+
+	return check[n][k] = (binomi(n - 1, k) + binomi(n - 1, k - 1)) % 10007;
+}
+int main() {
+	cin >> N >> K;
+	cout << binomi(N, K);
+}
