a2579494a7491b0413d2e4cd4d6632508ef1a05e
jingnee <jingnee721@gmail.com>
2020-12-13 18:34:03 +0900

    [BOJ]샘터 - 재채점 정답코드

--- baekjoon/18513.cpp
+++ baekjoon/18513.cpp
@@ -3,8 +3,6 @@
 #include <map>
 #include<queue>
 using namespace std;
-#define mini -100000000
-#define maxi 100000000
 
 int N, K;
 long long answer;
@@ -27,14 +25,14 @@ int main() {
 		int now = que.front().first;
 		int dist = que.front().second;
 		que.pop();
-		if (now - 1 >= mini && !isHave[now - 1]) {
+		if (!isHave[now - 1]) {
 			isHave[now - 1] = true;
 			K--;
 			answer += dist + 1;
 			que.push(make_pair(now - 1, dist + 1));
 			if (K == 0)break;
 		}
-		if (now + 1 <= maxi && !isHave[now + 1]) {
+		if (!isHave[now + 1]) {
 			isHave[now + 1] = true;
 			K--;
 			answer += dist + 1;
